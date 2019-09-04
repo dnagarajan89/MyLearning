aa339fd85c435d9014e1d3f29cdac501a25dbb02
Dhanesh <dnagarajan89@gmail.com>
2019-09-04 20:06:31 +0530

    Added web modules and default paths

--- nodejs/example-project/server.js
+++ nodejs/example-project/server.js
@@ -1,10 +1,28 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv')
+}
+
 const express = require('express')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
 
-const port = 5000
+const indexRouter = require('./routes/index')
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
 
-app.listen(port, () => console.log('Server is listening on port ' + port))
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+}, (err) => console.error(err))
+const db = mongoose.connection
+db.on('err', err => console.error(err))
+db.once('open', () => console.log("Connected to MongoDB"))
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is listening on port ' + (process.env.PORT || 3000)))
